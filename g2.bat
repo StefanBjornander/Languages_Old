ad98c1877f17f3caacd968b31e16ab7a32baf51a
Stefan Bjornander <stefan.bjornander@outlook.com>
2022-01-30 18:45:48 +0100

    Backup

--- g2.bat
+++ g2.bat
@@ -4,7 +4,9 @@
 
 @c:
 @cd "C:\Users\stefa\Documents\A A C_Compiler_CSharp_10"
-@git add .
+@git add C_Compiler_CSharp_10
+@git add Calculator_CS_10
+@git add C_Compiler_CSharp_10.sln
 @git commit -m "Backup"
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/CCompiler.git
@@ -12,7 +14,7 @@
 @rem pause
 
 @cd "C:\Users\Stefa\Documents\A A A C_Compiler_Assembler - CSharp 0 Assembly"
-@git add .
+@git add C_Compiler_CSharp_Assembly
 @git commit -m "Backup"
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/CCompiler_Assembly.git
@@ -20,7 +22,15 @@
 @rem pause
 
 @cd "C:\Users\Stefa\Documents\vagrant\homestead\code"
-@git add .
+@git add Test.result
+@git add code
+@git add code_old
+@git add code_very_old
+@git add objectivec
+@git add smalltalk-3.2
+@git add lua
+@git add haskell
+@git add scala
 @git commit -m "Backup"
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/Code.git
@@ -28,7 +38,12 @@
 @rem pause
 
 @cd "C:\Users\Stefa\Documents\GenerateAssembler"
-@git add .
+@git add GenerateAssembler
+@git add build
+@git add nbproject
+@git add src
+@git add build.xml
+@git add manifest.mf
 @git commit -m "Backup"
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/GenerateAssembler.git
@@ -36,7 +51,12 @@
 @rem pause
 
 @cd "C:\Users\stefa\Documents\ObjectCodeTableGeneratorCSharp"
-@git add .
+@git add nbproject
+@git add src
+@git add test
+@git add build
+@git add build.xml
+@git add manifest.mf
 @git commit -m "Backup"
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/ObjectCodeTableGeneratorCSharp.git
@@ -44,7 +64,12 @@
 @rem pause
 
 @cd "C:\Users\stefa\Documents\ObjectCodeTableGeneratorJava"
-@git add .
+@git add nbproject
+@git add src
+@git add test
+@rem git add build
+@git add build.xml
+@git add manifest.mf
 @git commit -m "Backup"
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/ObjectCodeTableGeneratorJava.git
@@ -52,7 +77,24 @@
 @rem pause
 
 @cd "C:\Users\stefa\Documents\Languages"
-@git add .
+@git add CPlusPlus
+@git add CSharp
+@git add Dart
+@git add Elixir
+@git add Erlang
+@git add Go
+@git add Haskell
+@git add Java
+@git add Julia
+@git add Kotlin
+@git add Python
+@git add Ruby
+@git add Rust
+@git add Scala
+@git add Swift
+@git add V
+@git add g.bat
+@git add languages.txt
 @git commit -m "Backup"
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/Languages.git
