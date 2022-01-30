e0d6fd0aac481326d912ab4e463a1df16955559f
Stefan Bjornander <stefan.bjornander@outlook.com>
2022-01-30 19:08:02 +0100

    Backup

--- g.bat
+++ g.bat
@@ -24,7 +24,6 @@
 @git commit -m "Backup"
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/Code.git
-@git pull origin master
 @git push origin master
 @rem pause
 
